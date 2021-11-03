73fe89024f5a28239c9bbe04bb01377ffe785e5a
bapatlaayyanna <bapatlaayyanna@gmail.com>
2021-11-03 20:31:56 +0530

    docstring updated

---
+++ utils/__init__.py
@@ -0,0 +1,2 @@
+print("Utils package ")
+__version__="0.1.1"
--- utils/all_utils.py
+++ utils/all_utils.py
@@ -5,6 +5,14 @@ import joblib
 import os
 
 def prepare_data(df):
+  """It used to separate dependent and independent data
+
+  Args:
+      df (Pandas DataFrame): Its data set
+
+  Returns:
+      tuples: it returns the tuples of dependent and independent variables
+  """
   X = df.drop("y", axis=1)
 
   y = df["y"]
@@ -12,7 +20,13 @@ def prepare_data(df):
   return X, y
 
 def save_model(model, filename):
+  """This method is to save model using joblib
+
+  Args:
+      model (python object): tarined model    
+      filename (str): path to save model  
+  """
   model_dir = "models"
-  os.makedirs(model_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
-  filePath = os.path.join(model_dir, filename) # model/filename
+  os.makedirs(model_dir, exist_ok=True) 
+  filePath = os.path.join(model_dir, filename) 
   joblib.dump(model, filePath)
